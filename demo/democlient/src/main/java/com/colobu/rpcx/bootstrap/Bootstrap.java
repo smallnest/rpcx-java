37dbf38e3a1021942f36b6ee6ea75ac086bfbd6c
zzy <zhangzhiyong1@xiaomi.com>
2018-10-22 14:37:38 +0800

    update

--- demo/democlient/src/main/java/com/colobu/rpcx/bootstrap/Bootstrap.java
+++ demo/democlient/src/main/java/com/colobu/rpcx/bootstrap/Bootstrap.java
@@ -1,5 +1,7 @@
 package com.colobu.rpcx.bootstrap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,8 +14,15 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = {"com.colobu.rpcx"})
 public class Bootstrap {
 
+    private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+
     public static void main(String... args) {
-        SpringApplication.run(Bootstrap.class, args);
+        try {
+            SpringApplication.run(Bootstrap.class, args);
+        } catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            System.exit(0);
+        }
     }
 
 }
