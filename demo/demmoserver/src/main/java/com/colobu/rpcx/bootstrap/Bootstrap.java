f707b76e72c6b71f070671c6dd76cd4cb2b44362
zzy <zhangzhiyong1@xiaomi.com>
2018-10-23 14:01:52 +0800

    update

--- demo/demmoserver/src/main/java/com/colobu/rpcx/bootstrap/Bootstrap.java
+++ demo/demmoserver/src/main/java/com/colobu/rpcx/bootstrap/Bootstrap.java
@@ -4,7 +4,6 @@ import com.colobu.rpcx.service.IArith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
